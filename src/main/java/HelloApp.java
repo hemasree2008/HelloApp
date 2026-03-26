67fa7383ef8cc271bf77945e7380b104c5d39283
hemasree2008 <hn9341@srmist.edu.in>
2026-03-26 11:01:09 +0530

    Update HelloApp.java

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,9 +1,21 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
+
+        String result;
+
+        if (args.length == 0) {
+            result = "World";
         } else {
-            System.out.println("Hello, World!");
+            StringBuilder sb = new StringBuilder();
+
+            for (String name : args) {
+                sb.append(name).append(", ");
+            }
+
+            // Remove last ", "
+            result = sb.substring(0, sb.length() - 2);
         }
+
+        System.out.println("Hello, " + result + "!");
     }
 }
